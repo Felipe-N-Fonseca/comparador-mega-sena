b5d6358d69f071ae6693da3d8743830464e1c1b5
Felipe do Nascimento Fonseca <felipenasfonseca@gmail.com>
2022-02-07 21:13:03 -0300

    2.0

--- libs.py
+++ libs.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def printf(msg,tam=40):
     print(f'{msg:^{tam}}')
 
@@ -8,3 +10,51 @@ def linha(tam=40):
 
 def colorido(cor='branco'):
     print('\033',end='')
+
+
+def criarq(arq):
+    try:
+        a = open(arq, 'rt')
+        a.close()
+    except FileNotFoundError:
+        try:
+            a = open(arq, 'wt+')
+            a.close()
+        except:
+            print('\033[1;31mERRO AO CRIAR ARQUIVO\033[m')
+        else:
+            print('\033[0;32mARQUIVO CRIADO COM SUCESSO\033[m')
+    else:
+        print('\033[0;32mARQUIVO ACESSADO COM SUCESSO\033[m')
+
+
+def escrevearq(arq, jogo):
+    a = open(arq , 'at')
+    data = datetime.now().date()
+    a.write(f'{jogo}:{data}\n')
+
+
+def conferedata(arq, data=datetime.now().date()):
+    jogos = []
+    jafoi = []
+    try:
+        a = open(arq, 'rt')
+    except:
+        print('\033[1;31mERRO AO LER ARQUIVO\033[m')
+    else:
+        for c in a:
+            b = c.replace('\n', '').split(':')
+            if b[1] == data:
+                jogos.append(b[0])
+            else:
+                jafoi.append(b[0])
+
+
+def numerosorteio():
+    from urllib import request
+
+    a = request.urlopen('http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/')
+'''    print('erro')
+    print(a.read())'''
+
+numerosorteio()
